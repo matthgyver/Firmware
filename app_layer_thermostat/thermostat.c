7339104d418ab7e6f036b8797b0ee3639788f71e
Jeremy Zongker <jeremy@trilitech.com>
2012-12-05 17:39:19 -0600

    Removed Delays
    The delay statements was causing the IOIO to loose it's connection with
    the android device when in override mode and not returning it.  I
    removed these delays and added TRY statements based on the example at
    http://www.di.unipi.it/~nids/docs/longjump_try_trow_catch.html

--- app_layer_thermostat/thermostat.c
+++ app_layer_thermostat/thermostat.c
@@ -1,11 +1,16 @@
 #define GetSystemClock()            32000000UL
 #define GetPeripheralClock()        (GetSystemClock())
 #define GetInstructionClock()       (GetSystemClock() / 2)
+#define TRY do{ jmp_buf ex_buf__; if( !setjmp(ex_buf__) ){
+#define CATCH } else {
+#define ETRY } }while(0)
+#define THROW longjmp(ex_buf__, 1)
 
 #include "thermostat.h"
 #include "timer.h"
 #include "adc.h"
 #include "features.h"
+#include <setjmp.h>
 
 
 int tempC_Tenths=60;
@@ -24,51 +29,62 @@ int tempOverride=0;
 
 void overTemp()
 {
-
-    SetPinDigitalOut_Override(h1Pin, 0, 0);
-    //SetPinDigitalOut_Override(h2Pin, 0, 0);
-    SetPinDigitalOut_Override(c1Pin, 1, 0);
+    TRY
+    {
+        SetPinDigitalOut_Override(h1Pin, 0, 0);
+        //SetPinDigitalOut_Override(h2Pin, 0, 0);
+        SetPinDigitalOut_Override(c1Pin, 1, 0);
+    } ETRY;
 }
 
 void underTemp()
 {
-
-    SetPinDigitalOut_Override(c1Pin, 0, 0);
-    //SetPinDigitalOut_Override(c2Pin, 0, 0);
-    //SetPinDigitalOut_Override(c3Pin, 0, 0);
-    SetPinDigitalOut_Override(h1Pin, 1, 0);
+    TRY {
+        SetPinDigitalOut_Override(c1Pin, 0, 0);
+        //SetPinDigitalOut_Override(c2Pin, 0, 0);
+        //SetPinDigitalOut_Override(c3Pin, 0, 0);
+        SetPinDigitalOut_Override(h1Pin, 1, 0);
+    } ETRY;
 }
 
 void allOff()
 {
-
-    SetPinDigitalOut_Override(h1Pin, 0, 0);
-    //SetPinDigitalOut_Override(h2Pin, 0, 0);
-    SetPinDigitalOut_Override(c1Pin, 0, 0);
-    //SetPinDigitalOut_Override(c2Pin, 0, 0);
-    //SetPinDigitalOut_Override(c3Pin, 0, 0);
+    TRY {
+        SetPinDigitalOut_Override(h1Pin, 0, 0);
+        //SetPinDigitalOut_Override(h2Pin, 0, 0);
+        SetPinDigitalOut_Override(c1Pin, 0, 0);
+        //SetPinDigitalOut_Override(c2Pin, 0, 0);
+        //SetPinDigitalOut_Override(c3Pin, 0, 0);
+    } ETRY;
 }
 
 
 
 int processRequest(int pin)
 {
-    //return 1;
-    if (pin==0 || pin==tempPin) return 1;
-    if (tempOverride==0) return 1; else return 0;
+    TRY {
+        //return 1;
+        if (pin==0 || pin==tempPin) return 1;
+        if (tempOverride==0) return 1; else return 0;
+    } ETRY;
+    return 0;
 }
 
 
 // Counts is 0 to 1023 indicating a step between 0v and 3.3v, 3.223mv per step.
 void setTemperature()
 {
-    steps = ReportChannelStatus(0);
+    TRY {
+        steps = ReportChannelStatus(0);
+    } ETRY;
 }
 
 void initThermostat()
 {
-//    ADCSetScan(tempPin,1);
-    SetPinAnalogIn(tempPin);
+    TRY {
+    //    ADCSetScan(tempPin,1);
+        SetPinAnalogIn(tempPin);
+    } ETRY;
 }
 
 void blink(int count)
@@ -82,7 +98,7 @@ void blink(int count)
         SetPinDigitalOut_Override(0, 0, 0);
         DelayMs(200);
     }
-    
+
 }
 
 
@@ -90,43 +106,44 @@ void blink(int count)
 //If temperature climbs above 95 turn off the heater and on the AC until 85.
 void safetyOverrideCheck()
 {
-    iteration++;
-    if (iteration>100)
-    {
-        //Only check every 100 loops to avoid unnecessary processing and rapid cycling.
-        iteration = 0;
-        setTemperature();
-        if (steps==0) return; //We don't have temperature sensor data.
-
-        //195 steps = 628mv, 12.8C, 55F
-        //248 steps = 800mv, 30C, 86F
-        if (tempOverride==1)
+    TRY {
+        iteration++;
+        if (iteration>100)
         {
-            if (steps>=195 && steps<=248) {
-                allOff();
-                tempOverride=0;
-                DelayMs(5000);
+            //Only check every 100 loops to avoid unnecessary processing and rapid cycling.
+            iteration = 0;
+            setTemperature();
+            if (steps==0) return; //We don't have temperature sensor data.
+
+            //195 steps = 628mv, 12.8C, 55F
+            //248 steps = 800mv, 30C, 86F
+            if (tempOverride==1)
+            {
+                if (steps>=195 && steps<=248) {
+                    allOff();
+                    //HardReset();
+                    tempOverride=0;
+                  //  DelayMs(5000);
+                }
+            }
+
+            if (tempOverride==0)
+            {
+                if (steps>=264)
+                {
+                    //264 steps = 850mv, 35C, 95F
+                    tempOverride=1;
+                    overTemp();
+                    //DelayMs(5000);
+
+                } else if (steps<=177)
+                {
+                    //177 steps = 570mv, 7C, 44.6F
+                    tempOverride=1;
+                    underTemp();
+                    //DelayMs(5000);
+                }
             }
         }
-
-        if (tempOverride==0)
-        {
-            if (steps>=264)
-            {
-                //264 steps = 850mv, 35C, 95F
-                tempOverride=1;
-                overTemp();
-                DelayMs(5000);
-
-            } else if (steps<=177)
-            {
-                //177 steps = 570mv, 7C, 44.6F
-                tempOverride=1;
-                underTemp();
-                DelayMs(5000);
-            }
-        }
-    }
+    } ETRY;
 }
-
-
